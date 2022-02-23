70566ef112d5b59a13a967efdae306bab4c8656b
Israel Fruchter <israel.fruchter@gmail.com>
2022-02-23 21:38:50 +0200

    Inital commit

---
+++ pytest_diff_selector/main.py
@@ -0,0 +1,167 @@
+import os.path
+import argparse
+import logging
+import sys
+from subprocess import check_output
+from collections import defaultdict
+from pathlib import Path
+from typing import Dict
+
+from unidiff import PatchSet, PatchedFile, Hunk, LINE_TYPE_ADDED, LINE_TYPE_REMOVED
+from pyan.analyzer import CallGraphVisitor, Flavor
+from tqdm import tqdm
+
+
+class CollectionVisitor(CallGraphVisitor):
+    def __init__(self, *args, **kwargs):
+        self.filenames_length = len(args[0])
+        self.progress_bar = tqdm(desc="Analyzing", total=self.filenames_length * 2)
+        super().__init__(*args, **kwargs)
+
+    def process(self):
+        self.defines_edges = defaultdict(list)
+        super().process()
+
+    def process_one(self, filename):
+        if self.progress_bar and self.progress_bar.disable:
+            print(f"scanning: {filename}")
+        super().process_one(filename)
+        if self.progress_bar:
+            self.progress_bar.set_postfix_str(filename)
+            self.progress_bar.update()
+
+    def postprocess(self):
+        """Finalize the analysis."""
+        self.resolve_imports()
+        if self.progress_bar:
+            self.progress_bar.close()
+
+
+def get_diff(git_repo_directory, git_selection) -> Dict[str, set]:
+    """
+    Get the unified diff from git, and return a mapping between file and
+    line number changed
+    """
+    diff = check_output(
+        ["git", "diff", "--no-prefix", git_selection], cwd=git_repo_directory, text=True
+    )
+    patch = PatchSet(diff)
+
+    print(patch, file=sys.stderr)
+
+    changed_lines = defaultdict(set)
+    for f in patch:
+        f: PatchedFile
+        for hunk in f:
+            hunk: Hunk
+            removed = {
+                l.source_line_no for l in hunk if l.line_type == LINE_TYPE_REMOVED
+            }
+            added = {l.target_line_no for l in hunk if l.line_type == LINE_TYPE_ADDED}
+            changed_lines[f.path] = changed_lines[f.path].union(removed, added)
+
+    return changed_lines
+
+
+class AffectedTestScanner:
+    """
+    scan the call graph to see which test is affected by the changes
+    """
+
+    def __init__(self, graph, changed_lines_set, root_path):
+        self.graph = graph
+        self.changed_lines_set = changed_lines_set
+        self.scanned_nodes = []
+        self.current_test = None
+        self.test_set = set()
+        self.root_path = root_path
+
+    def collect_tests(self) -> list:
+        for key_node, nodes in self.graph.uses_edges.items():
+            if key_node.name.startswith("test_"):
+                self.current_test = key_node
+                self.check_node_affected(key_node)
+                self.scan_nodes(nodes)
+                self.scanned_nodes.clear()
+
+        tests = []
+        for test in self.test_set:
+            relative_filename = Path(test.filename).relative_to(self.root_path)
+            namespace = []
+            for name in reversed(test.namespace.split(".")):
+                if name == relative_filename.name.rstrip(".py"):
+                    break
+                namespace += [name]
+            namespace = "::".join(reversed(namespace))
+            namespace = f"::{namespace}" if namespace else ""
+            test_full_name = f"{relative_filename}{namespace}::{test.name}"
+            tests.append(test_full_name)
+
+        return tests
+
+    def scan_nodes(self, nodes):
+        for node in nodes:
+            if node.flavor in [
+                Flavor.METHOD,
+                Flavor.CLASSMETHOD,
+                Flavor.STATICMETHOD,
+                Flavor.FUNCTION,
+            ]:
+                if self.check_node_affected(node):
+                    return True  # no point of continue if the test is already marked as affected
+                if node not in self.scanned_nodes:
+                    self.scanned_nodes.append(node)
+                    if node in self.graph.uses_edges:
+                        if self.scan_nodes(self.graph.uses_edges[node]):
+                            return True  # no point of continue if the test is already marked as affected
+            elif node.flavor == Flavor.IMPORTEDITEM:
+                if node not in self.scanned_nodes:
+                    self.scanned_nodes.append(node)
+                    if self.scan_nodes(self.graph.nodes[node.name]):
+                        return True  # no point of continue if the test is already marked as affected
+            else:
+                continue
+        return False
+
+    def check_node_affected(self, node):
+        if node.ast_node:
+            p = str(Path(node.filename).relative_to(self.root_path))
+            for line in self.changed_lines_set[p]:
+                if node.ast_node.lineno <= int(line) <= node.ast_node.end_lineno:
+                    self.test_set.add(self.current_test)
+                    return True
+        return False
+
+
+def run():
+    logging.basicConfig(level=logging.WARNING)
+
+    parser = argparse.ArgumentParser(description="Select tests based on git diff")
+    parser.add_argument(
+        "git_diff", default="HEAD", type=str, help="the parameter to pass to `git diff`"
+    )
+    parser.add_argument(
+        "--path", dest="root_path", default=".", help="the path of the git repo to scan"
+    )
+
+    args = parser.parse_args()
+    root_path = Path(os.path.abspath(args.root_path))
+    changed_lines_set = get_diff(root_path, args.git_diff)
+    files_changed = list(str(root_path / f) for f in changed_lines_set.keys())
+    files_changed = [f for f in files_changed if f.endswith(".py")]
+    if not files_changed:
+        print("No python file in the change/diff")
+        sys.exit()
+    files = list(str(f) for f in root_path.glob("**/*.py"))
+    graph = CollectionVisitor(files, str(root_path), logger=logging)
+
+    scanner = AffectedTestScanner(graph, changed_lines_set, root_path)
+    tests = scanner.collect_tests()
+    for test in tests:
+        print(test)
+
+    sys.exit()
+
+
+if __name__ == "__main__":
+    run()
---
+++ tests/test_cmd.py
@@ -0,0 +1,186 @@
+from pathlib import Path
+from textwrap import dedent
+
+import pytest
+from pytest_git import GitRepo
+
+
+selector = Path(__file__).parent.parent / "pytest_diff_selector" / "main.py"
+
+
+@pytest.fixture(scope="session")
+def session_git_repo():
+    """Session-scoped fixture to create a new git repo in a temporary workspace.
+
+    Attributes
+    ----------
+    uri (str) :  Repository URI
+    api (`git.Repo`) :  Git Repo object for this repository
+    .. also inherits all attributes from the `workspace` fixture
+
+    """
+    with GitRepo() as repo:
+        yield repo
+
+
+def write_file(repo, filename, content):
+    file = repo.workspace / filename
+    Path(file.parent).mkdir(exist_ok=True)
+    file.write_text(dedent(content))
+
+
+def append_file(repo, filename, content):
+    file = repo.workspace / filename
+    Path(file.parent).mkdir(exist_ok=True)
+    with file.open(mode="a") as f:
+        f.write(dedent(content))
+
+
+@pytest.fixture(scope="session")
+def test_repo(session_git_repo) -> GitRepo:
+    write_file(
+        session_git_repo,
+        "test_a.py",
+        """
+        from helpers import call_something
+        
+        class TestSomething:
+            def test_method():
+                global_var = global_var + 1
+                call_something()
+                assert 0/1
+                
+        def test_func1():
+            call_something()
+            
+            assert False
+    """,
+    )
+    session_git_repo.run("git add *.py")
+    session_git_repo.api.index.commit("Initial commit")
+    yield session_git_repo
+
+
+@pytest.fixture(scope="function", autouse=True)
+def clear_changes(test_repo):
+    test_repo.run("git stash", capture=True)
+
+
+def test_simple_scan(test_repo):
+    append_file(
+        test_repo,
+        "test_a.py",
+        """  
+        # comment 
+        def test_func2():
+            assert True
+    """,
+    )
+    ret = test_repo.run(f"python {selector} HEAD", capture=True)
+
+    assert "Analyzing: 100%" in ret
+
+
+def test_find_change_in_test_function(test_repo):
+    write_file(
+        test_repo,
+        "test_a.py",
+        """  
+        def test_func1():
+            call_something()
+            call_something_else()
+            
+            assert True
+            
+        # comment 
+        def test_func2():
+            assert True
+    """,
+    )
+    ret = test_repo.run(f"python {selector} HEAD", capture=True, shell=True)
+
+    assert "Analyzing: 100%" in ret
+    assert "test_a.py::test_func1" in ret
+
+
+def test_find_change_in_test_method(test_repo):
+    write_file(
+        test_repo,
+        "test_a.py",
+        """  
+        class TestSomething:
+            def test_method():
+                call_something()
+                call_something_else()
+                assert 0/1
+                
+        def test_func1():
+            call_something()
+            
+            assert False
+    """,
+    )
+    ret = test_repo.run(f"python {selector} HEAD", capture=True, shell=True)
+
+    assert "Analyzing: 100%" in ret
+    assert "test_a.py::TestSomething::test_method" in ret
+
+
+def test_find_change_not_in_test_file(test_repo):
+    write_file(
+        test_repo,
+        "helper.py",
+        """
+        def call_something():
+            print('doing')
+    """,
+    )
+    test_repo.run("git add *.py")
+    ret = test_repo.run(f"python {selector} HEAD", capture=True, shell=True)
+
+    assert "Analyzing: 100%" in ret
+    assert "test_a.py::test_func1" in ret
+    assert "test_a.py::TestSomething::test_method" in ret
+
+
+def test_find_change_not_in_test_file_nested(test_repo):
+    write_file(test_repo, "utils/__init__.py", """# just comment""")
+    write_file(
+        test_repo,
+        "utils/helper.py",
+        """
+        def call_something():
+            print('doing')
+    """,
+    )
+    test_repo.run("git add utils/*.py")
+
+    ret = test_repo.run(f"python {selector} HEAD", capture=True, shell=True)
+    assert "Analyzing: 100%" in ret
+    assert "test_a.py::test_func1" in ret
+    assert "test_a.py::TestSomething::test_method" in ret
+
+
+def test_find_change_in_test_module_scope(test_repo):
+    write_file(
+        test_repo,
+        "test_a.py",
+        """  
+        global_var = 20
+        
+        class TestSomething:
+            def test_method():
+                global_var = global_var + 1
+                call_something()
+                assert 0/1
+
+        def test_func1():
+            call_something()
+
+            assert False
+    """,
+    )
+    ret = test_repo.run(f"python {selector} HEAD", capture=True, shell=True)
+    print(ret)
+    assert "Analyzing: 100%" in ret
+    assert "test_a.py::TestSomething::test_method" in ret
